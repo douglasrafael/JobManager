1fddea90bcbf7e242f05660f4239f0d4d03ed85d
FS Developer <faelsantos.developer@outlook.com>
2016-05-01 00:33:33 -0300

    Implementação da class PhoneTypeDao

--- app/src/main/java/com/fsdeveloper/jobmanager/dao/PhoneTypeDao.java
+++ app/src/main/java/com/fsdeveloper/jobmanager/dao/PhoneTypeDao.java
@@ -24,7 +24,7 @@ public class PhoneTypeDao extends DBManager implements Dao<PhoneType> {
     };
 
     /**
-     * Class constructor, create instance of class DatabaseHelper.
+     * Class constructor.
      *
      * @param context Abstract class whose implementation is provided by Android system.
      */
